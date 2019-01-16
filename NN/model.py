72a3adb784568590a62f683c59a3f42142416ed3
satoshidg <satoshidg3104@gmail.com>
2019-01-15 23:04:20 -0800

    test

--- NN/model.py
+++ NN/model.py
@@ -9,7 +9,6 @@ class Model:
     def __init__(self):
         self.input_state = tf.placeholder(tf.float32, [None, 6, 7, 1]) #Where data comes in
         #the shape is [Batch_size, h, w, channel]
-        #TODO: shpae might have to be opposite, as in it could be 7, 6
 
         #Conv layer
         conv1 = tf.layers.conv2d(
@@ -31,6 +30,7 @@ class Model:
             )
 
         convs_out = tf.layers.Flatten()(conv2)
+        a = 1
 
         #Dense layers
         dense1 = tf.layers.dense(
@@ -90,13 +90,13 @@ class Model:
         self.sess = tf.Session()
         self.sess.run(self.init_OP)
 
-    def train(self, name):
+    def train(self, oppo_name):
         games_count = 0
-        oppo = Model()
 
+        oppo = Model() #This line initializes the opposite player for training
         try:
-            oppo = oppo.load(name)
-        except ValueError:
+            oppo.load(oppo_name) #if the model already exists, then load it
+        except ValueError: #Otherwise, do nothing and strat with blank model
             pass
 
         training_turn = 1
@@ -106,10 +106,19 @@ class Model:
         while games_count < pm.game_number: #keep training until it reaches epoch limit
             #Play batch number of games first
             D = list() #list to keep record:state, action, new state after action, reward
-            while len(D) < pm.batch_size:
+            while len(D) < pm.batch_size: #Keep playing the game until u have more than batch_size data
                 games_count += 1
                 env = Game()
                 game_status = 0 #Game's just started
+
+                #This if struct randomly decides which model plays first
+                if np.random.randint(0, 2) == 0:
+                    fir_player = self
+                    sec_player = oppo
+                else:
+                    fir_player = oppo
+                    sec_player = self
+
                 while(game_status == 0):
                     #player1
                     state, game_status = env.get_state(1)
@@ -117,11 +126,12 @@ class Model:
                         action = np.random.randint(0, 7, size=1)[0] #TODO: make sure 0 to 6 is randomly selected
                     else:
                         #get_state returns the board and game status (0 = undecided, 1 = player 1 won...)
-                        action = self.play(state)
+                        action = fir_player.play(state)
 
                     env.input(action, 1) #Execute the action as player 1
                     print("Player 1 - Action: " + str(action))
-                    env.display()
+                    if games_count % 100 == 0:
+                        env.display()
                     new_state, game_status = env.get_state(1) #Observe the env
                     new_data = [state, action, new_state]
                     #Append the reward to data list
@@ -140,11 +150,12 @@ class Model:
                         if np.random.rand() < pm.el:
                             action = np.random.randint(0, 7, size=1)[0]
                         else:
-                            action = oppo.play(state)
+                            action = sec_player.play(state)
 
                         env.input(action, 2) #Execute the action as player 2
                         print("Player 2 - Action: " + str(action))
-                        env.display()
+                        if games_count % 100 == 0:
+                            env.display()
                         new_state, game_status = env.get_state(2) #Observe the env
                         new_data = [state, action, new_state]
 
@@ -243,8 +254,8 @@ class Model:
         s_feed = self.reshape(s)
         Q = self.sess.run(self.out, feed_dict ={self.input_state: s_feed}) #TODO: Call the output of NN
         q_values = self.limit_actions(s, Q[0])
-        print("AI Made a decision: " + str(np.argmax(q_values)) + ", based on these values below")
-        print(q_values)
+        #print("AI Made a decision: " + str(np.argmax(q_values)) + ", based on these values below")
+        #print(q_values)
         return np.argmax(q_values)
 
     def close(self):
--- NN/parameter.py
+++ NN/parameter.py
@@ -3,6 +3,6 @@ batch_size = 10000 #batch_size = number of turns, not the games
 lr = 0.0003 #learning rate
 dr = 0.2 #dropout rate
 el = 0.3 #exploration rate
-filter_num = 6
+filter_num = 10
 num_actions = 7
 q_gamma = 0.3
